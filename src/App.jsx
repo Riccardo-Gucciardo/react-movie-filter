f0778d4ee65f51b9f4e00154f3d1ac10549ba80b
Riccardo-Gucciardo <riccardogucciardo003@gmail.com>
2025-02-26 15:53:04 +0100

    aggiunta useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -9,10 +9,23 @@ const titoList = [
   {id:6, title: 'Pulp Fiction', genre: 'Thriller' },
 ]
 
+
 function App() {
 
   const [filteredMovies,setFilteredMovies] = useState(titoList);
   const [genre,setGenre] = useState('')
+
+
+
+useEffect(() => {
+  if(genre !== ''){
+    const selectedMovies = titoList.filter((movie) => movie.genre === genre)
+    setFilteredMovies(selectedMovies)
+  }
+  else{
+    setFilteredMovies(titoList)
+  }
+},[genre])
 
 return(
   <>
@@ -23,12 +36,23 @@ return(
           filteredMovies.map((movie,index) =>{
             return(
               <li className="list-group-item" key={movie.id}>
-                {movie.title} {movie.genre}
+                {movie.title} - {movie.genre}
               </li>
+              
             )
           })
         }
       </ul>
+      
+      <div className="m-4">
+        <select className="form-control" name="" onChange={(e)=> setGenre(e.target.value)}>
+          <option value="">Tutti</option>
+          <option value="Fantascienza">Fantascienza</option>
+          <option value="Romantico">Romantico</option>
+          <option value="Azione">Azione</option>
+          <option value="Romantico">Romantico</option>
+        </select>
+      </div>
     
   </div>
   </>
